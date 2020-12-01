57883f42afafb5ecadc8e20ba83c03c5b40570e4
TavernaMo <frukt2903@gmail.com>
2020-12-01 14:17:48 +0300

    Lending Quiz, part 1

---
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import classes from './FinishedQuiz.module.scss'
+import Button from "../UI/Button/Button";
+const  FinishedQuiz = props => {
+    const successCount = Object.keys(props.results).reduce((sum,prev)=> {
+        if(props.results[prev]==='success'){
+            sum++
+        }
+        return sum
+    },0)
+    return(
+        <div className={classes.FinishedQuiz}>
+            <ul>
+                {props.quiz.map((quizItem,index)=>{
+                    const cls = [
+                        'fa',
+                        props.results[quizItem.id]=== 'error' ? 'fa-times' : 'fa-check',
+                        classes[props.results[quizItem.id]]
+                    ]
+                    return(
+                        <li key={index}>
+                            <strong>{index +1}</strong>. &nbsp;
+                            {quizItem.question}
+                            <i className={cls.join(' ')} />
+                        </li>
+                    )
+                })}
+
+            </ul>
+
+            <p>Правильно {successCount} из {props.quiz.length}</p>
+
+            <div>
+                <Button onClick={props.onRetry} type='primary'>Повторить</Button>
+                <Button  type='success'>Перейти в список текстов</Button>
+            </div>
+        </div>
+    )
+}
+
+export default FinishedQuiz
